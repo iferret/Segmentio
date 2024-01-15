b120a2994662e00759c0bb7d99440c65efcc11c4
iferret <wanggang3@staff.sina.com.cn>
2024-01-15 17:05:46 +0800

    update => no message

--- Package.swift
+++ Package.swift
@@ -1,14 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Segmentio",
     products: [
-        .library(name: "Segmentio", targets: ["Segmentio"]),
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Segmentio",
+            targets: ["Segmentio"]),
     ],
-    dependencies: [],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Segmentio",
             path: "./Segmentio/Source"
--- Segmentio/Source/Segmentio.swift
+++ Segmentio/Source/Segmentio.swift
@@ -9,6 +9,7 @@
 import UIKit
 import QuartzCore
 
+
 public typealias SegmentioSelectionCallback = ((_ segmentio: Segmentio, _ selectedSegmentioIndex: Int) -> Void)
 
 open class Segmentio: UIView {
